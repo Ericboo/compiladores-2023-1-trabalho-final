5ab6d6d6151cdceb5d29626738abdcf61989142c
Eric <eric43656@gmail.com>
2023-06-25 18:27:23 -0300

    Bugfix no tradutor

--- main.py
+++ main.py
@@ -4,20 +4,13 @@ from tradutor import translate_to_python
 
 
 source_code = """
-    fun getSum(a, b) {
-      return a + b;
-      var x = 1;
-    }
+    fun a1(a) { return a*2; }
+    fun a2(b) { return a/2; }
 
-    var sum = getSum(4, 5);
-
-    if(sum > 10) {
-    print "yes";
-    } else if(sum > 20) {
-    print "maybe";
-    } else {
-    print "no";
-    }
+    print a1(4) * a2(10); print a2(5) / a1(50);
+    var c = nil;
+    if(a1(4) > 5) { print "ok"; }
+    c = 40; print c;
 
     """
 
